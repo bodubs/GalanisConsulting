533e79d0ac8371e2d9dcd9b5ad7bacd67ee3a1b5
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-09-15 18:59:19 -0400

    Added CSS to projects page

--- projects.php
+++ projects.php
@@ -22,7 +22,12 @@ include 'top.php';
                 </div>
                 <div class="card">
                     <a href="andover_commons.php">
-                        <img src="images/projects/andover_commons.jpg" alt="Phillips Academy Commons" class="projects-img"/>
+                        <div class="content-container">
+                            <img src="images/projects/andover_commons.jpg" alt="Phillips Academy Commons" class="projects-img"/>
+                            <div class="view-project-middle">
+                                <div class="view-project-text">View Project</div>
+                            </div>
+                        </div>
                         <div class="proj-img-text-div">
                             <p>Andover Commons</p>
                             <p>Phillips Academy</p>
@@ -30,39 +35,74 @@ include 'top.php';
                     </a>
                 </div>
                 <div class="card">
-                    <img src="images/projects/boat_house.jpg" alt="Phillips Academy Boat House" class="projects-img"/>
-                    <div class="proj-img-text-div">
-                        <p>Boat House</p>
-                        <p>Phillips Academy</p>
-                    </div>
+                    <a href="andover_boat_house.php">
+                        <div class="content-container">
+                            <img src="images/projects/boat_house.jpg" alt="Phillips Academy Boat House" class="projects-img"/>
+                            <div class="view-project-middle">
+                                <div class="view-project-text">View Project</div>
+                            </div>
+                        </div>
+                        <div class="proj-img-text-div">
+                            <p>Boat House</p>
+                            <p>Phillips Academy</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="card">
-                    <img src="images/projects/Bulfinch1.JPG" alt="Phillips Academy Bulfinch Hall" class="projects-img"/>
-                    <div class="proj-img-text-div">
-                        <p>Bulfinch Hall</p>
-                        <p>Phillips Academy</p>
-                    </div>
+                    <a href="bulfinch.php">
+                        <div class="content-container">
+                            <img src="images/projects/Bulfinch1.JPG" alt="Phillips Academy Bulfinch Hall" class="projects-img"/>
+                            <div class="view-project-middle">
+                                <div class="view-project-text">View Project</div>
+                            </div>
+                        </div>
+                        <div class="proj-img-text-div">
+                            <p>Bulfinch Hall</p>
+                            <p>Phillips Academy</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="card">
-                    <img src="images/projects/owh_library.jpg" alt="Phillips Academy Library Inn" class="projects-img"/>
-                    <div class="proj-img-text-div">
-                        <p>Oliver Wendell Holmes Library</p>
-                        <p>Phillips Academy</p>
-                    </div>
+                    <a href="owh_library.php">
+                        <div class="content-container">
+                            <img src="images/projects/owh_library.jpg" alt="Phillips Academy Oliver Wendell Holmes Library" class="projects-img"/>
+                            <div class="view-project-middle">
+                                <div class="view-project-text">View Project</div>
+                            </div>
+                        </div>
+                        <div class="proj-img-text-div">
+                            <p>Oliver Wendell Holmes Library</p>
+                            <p>Phillips Academy</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="card">
-                    <img src="images/projects/snyder_center.jpg" alt="Phillips Academy Snyder Athletic Center" class="projects-img"/>
-                    <div class="proj-img-text-div">
-                        <p>Snyder Athletic Center</p>
-                        <p>Phillips Academy</p>
-                    </div>
+                    <a href="snyder.php">
+                        <div class="content-container">
+                            <img src="images/projects/snyder_center.jpg" alt="Phillips Academy Snyder Athletic Center" class="projects-img"/>
+                            <div class="view-project-middle">
+                                <div class="view-project-text">View Project</div>
+                            </div>
+                        </div>
+                        <div class="proj-img-text-div">
+                            <p>Snyder Athletic Center</p>
+                            <p>Phillips Academy</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="card">
-                    <img src="images/projects/brooks_chapel_sketch.jpg" alt="Brooks School Ashburn Chapel" class="projects-img"/>
-                    <div class="proj-img-text-div">
-                        <p>Ashburn Chapel</p>
-                        <p>Brooks School</p>
-                    </div>
+                    <a href="ashburn_chapel.php">
+                        <div class="content-container">
+                            <img src="images/projects/brooks_chapel_sketch.jpg" alt="Brooks School Ashburn Chapel" class="projects-img"/>
+                            <div class="view-project-middle">
+                                <div class="view-project-text">View Project</div>
+                            </div>
+                        </div>
+                        <div class="proj-img-text-div">
+                            <p>Ashburn Chapel</p>
+                            <p>Brooks School</p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </section>
