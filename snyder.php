10c7d188bc1fc654f11d8b6ba765c6de9b9ac442
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-09-15 20:50:54 -0400

    Added more project pages

---
+++ snyder.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'top.php';
+
+?>
+
+<div class="projects-detail-container">
+
+	<h2>Phillips Academy Snyder Athletic Center</h2>
+	<h3>Andover, Massachusetts</h3><br>
+
+	<p><strong>Client</strong><br>
+		Phillips Academy Andover</p><br>
+
+	<p><strong>Project Value</strong><br>
+		$39 million</p><br>
+
+	<p><strong>Completion</strong><br>
+		2017</p><br>
+
+	<p><strong>General Contractor</strong><br>
+		Erland Construction</p><br>
+
+	<p><strong>Architect</strong><br>
+		Perkins & Will Architects</p><br>
+
+	<p>Designed by Perkins & Will Architects, the 98,800 square feet, Phillips Academy’s state-of-the-art Snyder Center holds a LEED (Leadership and Energy Environmental Design) Platinum designation.<br><br>The multi-sport field house facility incorporates a 200-meter competition NCAA standards track; a 12-court squash center including a glass enclosed exhibition court, recreation space, meeting rooms, and accommodations for up to 2,500 people for campus functions.<br><br>The waste heat from the ice making process is used to heat the new building, thus reducing the energy use of the Snyder Center by half.  The general contractor coordinated the replacement of the refrigeration system while the ice arena was in constant use and installed significant underground piping between two facilities through an already crowded utility corridor. The success of that effort was critical to the project achieving a net zero energy design.</p>
+
+</div>
+
+<?php
+
+include 'footer.php';
+
+?>
