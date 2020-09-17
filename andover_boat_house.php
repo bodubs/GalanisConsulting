8be5d82995f0ceb9e58148995c4266fe01e58097
Jake Warren <jwarshack@gmail.com>
2020-09-17 17:48:01 -0400

    project carousel added to boat-house page

--- andover_boat_house.php
+++ andover_boat_house.php
@@ -24,9 +24,97 @@ include 'top.php';
 	<p><strong>Architect</strong><br>
 		Architectural Resources Cambridge</p><br>
 
-	<p>In 2010 Galanis Consulting assisted Phillips Academy in the purchasing of 5.8 acres of property along the banks of the Merrimack River in Methuen. The site at 620 Lowell Street served as the former General GMC truck dealership housing over 100 vehicles on the property.<br><br>The Academy refurbished the site for use by its varsity and instructional crew programs. The William H. Brown Boathouse was created by renovating an existing, prefabricated metal building dating from the 1970s. The building was transformed to its new use with a handful of modest upgrades to the existing structure, including new metal panel siding, an epoxy roof, and new oversized glass openings to maximize natural light coming into the building. The double-height great room forms the hub of activity on race-day events, with new glazed openings along its northern and eastern sides providing spectators with the opportunity to watch the races from indoors. A new boat launch area and docks were added along the Merrimac River.<br><br>Permitting was extensive with coordination conducted with the Army Corps of Engineers; Methuen Conservation Commission; Mass DEP; EPA; Massachusetts Fisheries Division.</p>
+	<div class="project-carousel-container">
+		<div class="project-carousel">
+			<div class="project-nav-arrow" id="arrow-left"><</div>
+			<div class="project-slide fade">
+				<img src="images/projects/boat_house.jpg" alt="">
+			</div>
+			<div class="project-slide fade">
+				<img src="images/projects/boat-house2.jpg" alt="">
+			</div>
+			<div class="project-slide fade">
+				<img src="images/projects/boat-house3.jpg" alt="">
+			</div>
+			<div class="project-slide fade">
+				<img src="images/projects/boat-house4.jpg" alt="">
+			</div>
+			<div class="project-nav-arrow" id="arrow-right">></div>
+		</div>
+
+		<div class="thumbnail-track">
+			<div class="thumbnail">
+				<img src="images/projects/boat_house.jpg" alt="">
+			</div>
+			<div class="thumbnail">
+				<img src="images/projects/boat-house2.jpg" alt="">
+			</div>
+			<div class="thumbnail">
+				<img src="images/projects/boat-house3.jpg" alt="">
+			</div>
+			<div class="thumbnail">
+				<img src="images/projects/boat-house4.jpg" alt="">
+			</div>
+		</div>
+
+	</div>
+
+
+
+<p>In 2010 Galanis Consulting assisted Phillips Academy in the purchasing of 5.8 acres of property along the banks of the Merrimack River in Methuen. The site at 620 Lowell Street served as the former General GMC truck dealership housing over 100 vehicles on the property.<br><br>The Academy refurbished the site for use by its varsity and instructional crew programs. The William H. Brown Boathouse was created by renovating an existing, prefabricated metal building dating from the 1970s. The building was transformed to its new use with a handful of modest upgrades to the existing structure, including new metal panel siding, an epoxy roof, and new oversized glass openings to maximize natural light coming into the building. The double-height great room forms the hub of activity on race-day events, with new glazed openings along its northern and eastern sides providing spectators with the opportunity to watch the races from indoors. A new boat launch area and docks were added along the Merrimac River.<br><br>Permitting was extensive with coordination conducted with the Army Corps of Engineers; Methuen Conservation Commission; Mass DEP; EPA; Massachusetts Fisheries Division.</p>
+
 
 </div>
+
+
+
+<script>
+
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+	updateThumbnails();
+
+	function showSlides(n){
+		var i;
+		const slides = document.getElementsByClassName("project-slide");
+		if (n > slides.length) {slideIndex = 1}
+		if (n < 1) {slideIndex = slides.length}
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+
+		slides[slideIndex-1].style.display = "block";
+	}
+
+	function updateThumbnails(n){
+		const thumbnails = document.getElementsByClassName("thumbnail");
+
+		for (var i = 0; i < thumbnails.length; i++){
+			if(thumbnails[i].classList.contains("selected")){
+				console.log("This is selected");
+				thumbnails[i].classList.remove("selected");
+			}
+		}
+
+		thumbnails[slideIndex-1].classList.add("selected");
+
+
+
+
+	}
+
+	document.getElementById("arrow-left").addEventListener("click", e =>{
+		showSlides(slideIndex += -1);
+		updateThumbnails();
+	})
+
+	document.getElementById("arrow-right").addEventListener("click", e =>{
+		showSlides(slideIndex += 1);
+		updateThumbnails();
+	})
+</script>
+
 
 <?php
 
