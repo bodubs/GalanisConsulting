47fa8fc66548857fef2fa25cb6810e23312074bd
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-09-17 10:27:47 -0400

    Added clients page

---
+++ clients.php
@@ -0,0 +1,41 @@
+<?php
+
+include 'top.php';
+
+?>
+    <div class="container">
+
+        <h1 class="pages-title">Clients</h1>
+
+        <section class="projects">
+            <div class="card-container">
+                <div class="card">
+                    <a href="andover_inn.php">
+                        <img src="images/logos/brooks-logo.png" alt="Brooks School Logo" class="client-logos"/>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="andover_inn.php">
+                        <img src="images/logos/bbn-logo.png" alt="Buckingham Browne & Nichols Logo" class="client-logos"/>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="andover_inn.php">
+                        <img src="images/logos/phillips-logo.png" alt="Phillips Academy Andover Logo" class="client-logos"/>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="andover_inn.php">
+                        <img src="images/logos/pingree-logo.png" alt="Pingree School Logo" class="client-logos"/>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    
+<?php
+
+include 'footer.php';
+
+?>
--- header.php
+++ header.php
@@ -9,7 +9,7 @@
                 <ul class="right-nav">
                     <li><a href="index.php">Home</a></li>
                     <li><a href="about.php">About</a></li>
-                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="clients.php">Clients</a></li>
                     <li><a href="awards.php">Awards</a></li>
                     <li><a href="contact.php">Contact</a></li>
                 </ul>
