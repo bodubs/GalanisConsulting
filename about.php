a5ce5ceba54c52ce4efac105dd7c39ef59cb199c
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-09-21 18:38:07 -0400

    Worked on about page

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
 <section class="about">
     <div class="container">
         <div class="bio-container">
-            <img class="chaz" src="./images/yani.jpg" alt="">
+            <img id="yani" src="images/yani.jpg" alt="John Galanis Portrait">
             <p>John Galanis has more than 30 years experience in the project planning and construction 
                 management of academic, historic, institutional, commercial, industrial, casino, hotel, 
                 and residential structures working directly for building owners and institutions. 
@@ -16,6 +16,16 @@
                 million in construction projects working firectly for owners.
             </p>
         </div>
+    </div>
+</section>
+
+<section id="education">
+    <div class="container">
+        <img src="images/logos/tufts-logo.png" alt="Tufts Logo" id="tufts-logo" />
+        <h1>Education</h1><br>
+        <p><strong>Tufts University</strong></p>
+        <p>BSCE, Civil Engineering</p>
+        <p>1978-1982</p>
     </div>
 </section>
 
