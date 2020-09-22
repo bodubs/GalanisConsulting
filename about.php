70d7e1d71ff06e1093f7aaa20b1c23ae8fa9a322
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-09-22 07:45:47 -0400

    Working on about page

--- about.php
+++ about.php
@@ -3,11 +3,19 @@
 
 <h1 class="pages-title">About</h1>
 
+<section id="about-llc">
+    <div class="container">
+        <div class="about-llc-container">
+            <p>Galanis Consulting LLC was started in 1999 by John Galanis. Projects range from Academic to Private Development and anywhere in between.</p>
+        </div>
+    </div>
+</section>
+
 <section class="about">
     <div class="container">
         <div class="bio-container">
             <img id="yani" src="images/yani.jpg" alt="John Galanis Portrait">
-            <p>John Galanis has more than 30 years experience in the project planning and construction 
+            <p>John Galanis has more than 37 years experience in the project planning and construction 
                 management of academic, historic, institutional, commercial, industrial, casino, hotel, 
                 and residential structures working directly for building owners and institutions. 
                 His various supervisory positions from "hands on" supervision of field staff to the role 
@@ -21,11 +29,18 @@
 
 <section id="education">
     <div class="container">
-        <img src="images/logos/tufts-logo.png" alt="Tufts Logo" id="tufts-logo" />
+        
         <h1>Education</h1><br>
-        <p><strong>Tufts University</strong></p>
-        <p>BSCE, Civil Engineering</p>
-        <p>1978-1982</p>
+        <div id="edu-div">
+            <div>
+                <p><strong>Tufts University</strong></p>
+                <p>Bachelor of Science in Civil Engineering</p>
+                <p>1978-1982</p>
+            </div>
+            <div>
+                <img src="images/logos/tufts-logo.png" alt="Tufts Logo" id="tufts-logo" />
+            </div>
+        </div>
     </div>
 </section>
 
