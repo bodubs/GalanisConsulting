79714fa9f1581f987fb5cf89cc112013b6c76706
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-11-12 13:48:07 -0500

    Applied pics to projects pages

--- owh_library.php
+++ owh_library.php
@@ -9,6 +9,8 @@ include 'top.php';
 	<h2>Phillips Academy Oliver Wendell Holmes Library</h2>
 	<h3>Andover, Massachusetts</h3><br>
 
+	<img src="images/projects/owh_library.jpg" alt="OWH Library" id="andover-inn-main-img">
+
 	<p><strong>Client</strong><br>
 		<a href="https://www.andover.edu/">Phillips Academy Andover</a></p><br>
 
@@ -23,6 +25,17 @@ include 'top.php';
 
 	<p><strong>Architect</strong><br>
 		<a href="https://www.annbeha.com/">Ann Beha Architects</a></p><br>
+
+
+	<div class="project-pics">
+
+		<img src="images/projects/owh_library2.jpg" alt="OWH Library Pillars" class="proj-img-solo"/>
+
+		<img src="images/projects/owh_library3.jpg" alt="OWH Library Hallway" class="proj-img-solo"/>
+
+		<img src="images/projects/owh_library4.jpg" alt="OWH Library Board" class="proj-img-solo"/>
+
+	</div>
 
 	<p>Phillips Academy’s first modern library was funded by Thomas Cochran, Class of 1890, who helped orchestrate the physical transformation of campus during that period. Designed by New York architect Charles A. Platt, the OWHL, as it affectionately became known, was named after Holmes Sr., the renowned physician and poet.<br><br>Through the years, Andover’s library has expanded and evolved with the changing needs of its students. The 2019 project transformed the existing library to an integrated learning hub, responsive to the changing ways in which students access information, encouraging new styles of inquiry, learning, research, and collaboration. Design firm Ann Beha Associates has preserved the historic spaces, repurposed support spaces. The former 5 story book stacks were removed, condensed and relocated within the building architecture. They have been replaced by collaborative study and learning spaces, including a global innovations studio. Archival resources are moved to the main floor, giving them a new and accessible presence, joined with Knafel map center. The Sidney R. Knafel Map Collection is history itself in the form of a unique collection of atlases, maps, and globes dating from 1434 well into the 19th century. Many of these rare objects and documents are the subject of a 1992 book, Shedding the Veil: Mapping the European Discovery of America and the World by Thomas Suárez, and comprised in that year an exhibit at the Addison Gallery on the campus of Phillips Academy to mark the 500th anniversary of Columbus’ landing in the New World.<br><br>Restoration of a Stuart Travis Mural took place within the Freeman Room of the OWHL. Travis was an accomplished and prolific American artist, illustrator, and designer who studied at the Académie Julian in Paris. His works—mostly drawings and watercolors—appeared frequently in magazines, books, and advertisements in the early twentieth century.<br><br>This work – along with a new café, makerspace, digital lab, wood shop, 3-D laser printing, robotics, and a 50% increase in seating – create new academic synergies with vision and spirit.</p>
 
--- snyder.php
+++ snyder.php
@@ -4,13 +4,12 @@ include 'top.php';
 
 ?>
 
-<div class="proj-header" id="snyder-header">
+<div class="projects-detail-container">
 
 	<h2>Phillips Academy Snyder Athletic Center</h2>
 	<h3>Andover, Massachusetts</h3>
 
-</div>
-<div class="projects-detail-container">
+	<img src="images/projects/snyder_center.jpg" alt="Snyder Center" id="andover-inn-main-img">
 
 	<p><strong>Client</strong><br>
 		<a href="https://www.andover.edu/">Phillips Academy Andover</a></p><br>
@@ -26,6 +25,16 @@ include 'top.php';
 
 	<p><strong>Architect</strong><br>
 		<a href="https://perkinswill.com/">Perkins & Will Architects</a></p><br>
+
+	<div class="project-pics">
+
+		<img src="images/projects/snyder_center2.jpg" alt="Snyder Center Court" class="proj-img-solo"/>
+
+		<img src="images/projects/snyder_center3.jpg" alt="Snyder Center Solar Panels" class="proj-img-solo"/>
+
+		<img src="images/projects/snyder_center4.jpg" alt="Snyder Center Court Demo" class="proj-img-solo"/>
+
+	</div>
 
 	<p>Designed by Perkins & Will Architects, the 98,800 square feet, Phillips Academy’s state-of-the-art Snyder Center holds a LEED (Leadership and Energy Environmental Design) Platinum designation.<br><br>The multi-sport field house facility incorporates a 200-meter competition NCAA standards track; a 12-court squash center including a glass enclosed exhibition court, recreation space, meeting rooms, and accommodations for up to 2,500 people for campus functions.<br><br>The waste heat from the ice making process is used to heat the new building, thus reducing the energy use of the Snyder Center by half.  The general contractor coordinated the replacement of the refrigeration system while the ice arena was in constant use and installed significant underground piping between two facilities through an already crowded utility corridor. The success of that effort was critical to the project achieving a net zero energy design.</p>
 
