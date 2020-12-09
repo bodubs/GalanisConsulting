8044ff9d06058878867509e6f2c2fb06e9df4e37
Jake Warren <jwarshack@gmail.com>
2020-12-08 19:12:16 -0500

    Projects in chronological order

--- projects.php
+++ projects.php
@@ -9,36 +9,62 @@ include 'top.php';
 
         <fieldset class="projects pages-sect">
             <div class="year-container">
-                <h1>2010</h1>
+                <h1>2019</h1>
                 <div class="card-container">
-                    <div class="card" id="andover-inn-proj-card">
-                        <a href="andover_inn.php">
+                    <div class="card" id="owh-library-proj-card">
+                        <a href="owh_library.php">
                             <div class="content-container">
-                                <img src="images/projects/andover_inn.jpg" alt="Andover Inn" class="projects-img"/>
+                                <img src="images/projects/owh_library.jpg" alt="Phillips Academy Oliver Wendell Holmes Library" class="projects-img"/>
                                 <div class="view-project-middle">
                                     <div class="view-project-text">View Project</div>
                                 </div>
                             </div>
                             <div class="proj-img-text-div">
-                                <p>Andover Inn</p>
+                                <p>Oliver Wendell Holmes Library</p>
                                 <p>Phillips Academy</p>
                             </div>
                         </a>
                     </div>
-                    <div class="card" id="andover-commons-proj-card">
-                        <a href="andover_commons.php">
+                </div>
+            </div>
+            <div class="year-container">
+                <h1>2017</h1>
+                <div class="card-container">
+                    <div class="card" id="snyder-proj-card">
+                        <a href="snyder.php">
                             <div class="content-container">
-                                <img src="images/projects/andover_commons.jpg" alt="Phillips Academy Commons" class="projects-img"/>
+                                <img src="images/projects/snyder_center.jpg" alt="Phillips Academy Snyder Athletic Center" class="projects-img"/>
                                 <div class="view-project-middle">
                                     <div class="view-project-text">View Project</div>
                                 </div>
                             </div>
                             <div class="proj-img-text-div">
-                                <p>Andover Commons</p>
+                                <p>Snyder Athletic Center</p>
                                 <p>Phillips Academy</p>
                             </div>
                         </a>
                     </div>
+
+                </div>
+            </div>
+            <div class="year-container">
+                <h1>2015</h1>
+                <div class="card-container">
+                    <div class="card" id="ashburn-chapel-proj-card">
+                        <a href="ashburn_chapel.php">
+                            <div class="content-container">
+                                <img src="images/projects/brooks_chapel_sketch.jpg" alt="Brooks School Ashburn Chapel" class="projects-img"/>
+                                <div class="view-project-middle">
+                                    <div class="view-project-text">View Project</div>
+                                </div>
+                            </div>
+                            <div class="proj-img-text-div">
+                                <p>Ashburn Chapel</p>
+                                <p>Brooks School</p>
+                            </div>
+                        </a>
+                    </div>
+
                 </div>
             </div>
             <div class="year-container">
@@ -76,64 +102,42 @@ include 'top.php';
                 </div>
             </div> 
             <div class="year-container">
-                <h1>2015</h1>
+                <h1>2010</h1>
                 <div class="card-container">
-                    <div class="card" id="ashburn-chapel-proj-card">
-                        <a href="ashburn_chapel.php">
+                    <div class="card" id="andover-inn-proj-card">
+                        <a href="andover_inn.php">
                             <div class="content-container">
-                                <img src="images/projects/brooks_chapel_sketch.jpg" alt="Brooks School Ashburn Chapel" class="projects-img"/>
+                                <img src="images/projects/andover_inn.jpg" alt="Andover Inn" class="projects-img"/>
                                 <div class="view-project-middle">
                                     <div class="view-project-text">View Project</div>
                                 </div>
                             </div>
                             <div class="proj-img-text-div">
-                                <p>Ashburn Chapel</p>
-                                <p>Brooks School</p>
-                            </div>
-                        </a>
-                    </div>
-
-                </div>
-            </div>
-            <div class="year-container">
-                <h1>2017</h1>
-                <div class="card-container">
-                    <div class="card" id="snyder-proj-card">
-                        <a href="snyder.php">
-                            <div class="content-container">
-                                <img src="images/projects/snyder_center.jpg" alt="Phillips Academy Snyder Athletic Center" class="projects-img"/>
-                                <div class="view-project-middle">
-                                    <div class="view-project-text">View Project</div>
-                                </div>
-                            </div>
-                            <div class="proj-img-text-div">
-                                <p>Snyder Athletic Center</p>
+                                <p>Andover Inn</p>
                                 <p>Phillips Academy</p>
                             </div>
                         </a>
                     </div>
-
-                </div>
-            </div>
-            <div class="year-container">
-                <h1>2019</h1>
-                <div class="card-container">
-                    <div class="card" id="owh-library-proj-card">
-                        <a href="owh_library.php">
+                    <div class="card" id="andover-commons-proj-card">
+                        <a href="andover_commons.php">
                             <div class="content-container">
-                                <img src="images/projects/owh_library.jpg" alt="Phillips Academy Oliver Wendell Holmes Library" class="projects-img"/>
+                                <img src="images/projects/andover_commons.jpg" alt="Phillips Academy Commons" class="projects-img"/>
                                 <div class="view-project-middle">
                                     <div class="view-project-text">View Project</div>
                                 </div>
                             </div>
                             <div class="proj-img-text-div">
-                                <p>Oliver Wendell Holmes Library</p>
+                                <p>Andover Commons</p>
                                 <p>Phillips Academy</p>
                             </div>
                         </a>
                     </div>
                 </div>
             </div>
+
+
+
+
 
 
 
