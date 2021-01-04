78914fbe2cc8b93975e5d687ff002a8938ba6229
Robert WarrenIII <robert.warren-iii@uvm.edu>
2021-01-04 13:06:46 -0500

    Started on beacon

---
+++ beacon.php
@@ -0,0 +1,56 @@
+<?php
+
+include 'top.php';
+
+?>
+
+<div class="projects-detail-container">
+
+	<h2>The Appleton-Parker House, 39-40 Beacon Street</h2>
+	<h3>Boston, Massachusetts</h3><br>
+
+	<img src="images/projects/beacon1.jpg" alt="40 Beacon Street" id="andover-inn-main-img">
+
+	<p><strong>Client</strong><br>
+		<a href="http://www.papent.com/">Pappas Enterprises, INC</a></p><br>
+
+	<p><strong>Project Value</strong><br>
+		$12.5 million</p><br>
+
+	<p><strong>Building Size</strong><br>
+		17,000 square feet</p><br>
+
+	<p><strong>Completion</strong><br>
+		2016</p><br>
+
+	<p><strong>General Contractor</strong><br>
+		<a href="https://www.consigli.com/">Consigli Construction</a></p><br>
+
+	<p><strong>Architect</strong><br>
+		<a href="https://www.austinarchitects.com/">Austin Architects</a></p><br>
+
+	<div class="project-pics">
+
+		<img src="images/projects/bbnms2.jpg" alt="Buckingham Browne & Nichols Middle School" class="proj-img-solo"/>
+
+		<img src="images/projects/bbnms3.jpg" alt="Buckingham Browne & Nichols Middle School" class="proj-img-solo"/>
+
+		<img src="images/projects/bbnms4.jpg" alt="Buckingham Browne & Nichols Middle School" class="proj-img-solo"/>
+
+	</div>
+
+	<p>In 2011, BB&N’s Board of Trustees engaged Austin Architects to determine how best to address a number of significant programmatic needs and infrastructure challenges on their Middle School campus. The architects worked with faculty, administrators, and trustees to develop a comprehensive plan to address those needs and in 2015 implemented the plan with the beginning of construction.<br><br>The Musgrave Mansion is framed by two graceful beech trees that distinguish this property. The plan leverages and preserves these assets, both internal and external. By dropping the grade on the south side of Musgrave, a new main entry and terrace was created on the lower level. A new glass façade and staircase unites the Musgrave and Vaillant Buildings, with light-filled student foyers clustering around the Grand Stairway. The community gathering space (the Big Room) gains a gracious reception foyer, as does the main entry.<br><br>The renovation also captures an additional 3,240 square feet, formerly the basement of the Musgrave Building, for academic and community use. Seven classrooms are enlarged or relocated to more appropriate spaces, with a net gain of one classroom. Four faculty offices, the faculty room, and the Director’s office are improved, with many of these relocated to spaces more appropriate to their function, as is the admission office. Two additional conference rooms are included in the floor plans as are two small group meeting rooms. The Middle School Library moves to the entry level where it joins with technology to create a 1,600-square-foot Learning Commons that becomes a student and faculty hub for information, assisted research, and technology support.<br><br>New infrastructure systems include an elevator that services all levels; dramatically increased internet bandwidth, access, and connectivity speed; upgrades to the sprinkler, fire alarm, and electrical systems; an energy-efficient boiler; as well as upgrades to all interior finishes and exterior surfaces.<br><br></p>
+
+
+	<h3>Asscociated Links:</h3>
+	<p><a href="http://www.papent.com/">http://www.papent.com/</a></p>
+	<p><a href="https://www.consigli.com/">https://www.consigli.com/</a></p>
+	<p><a href="https://www.austinarchitects.com/">https://www.austinarchitects.com/</a></p>
+
+</div>
+
+<?php
+
+include 'footer.php';
+
+?>
