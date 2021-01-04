7e3643266dfa22e1899976144b6fb26a9efb9d5d
Robert WarrenIII <robert.warren-iii@uvm.edu>
2021-01-04 13:18:08 -0500

    Editing about page

--- about.php
+++ about.php
@@ -15,13 +15,13 @@
     <div class="container">
         <div class="bio-container">
             <img id="yani" src="images/yani.jpg" alt="John Galanis Portrait">
-            <p>John Galanis has more than 37 years experience in the project planning and construction 
+            <p>John Galanis has more than 40 years experience in the project planning and construction 
                 management of academic, historic, institutional, commercial, industrial, casino, hotel, 
                 and residential structures working directly for building owners and institutions. 
                 His various supervisory positions from "hands on" supervision of field staff to the role 
                 of Project Manager have allowed him to provide cost effective analysis and value to a 
-                wide variety of projects. In the past 13 years Galanis Consulting has managed over $220 
-                million in construction projects working firectly for owners.
+                wide variety of projects. In the past 20 years Galanis Consulting has managed over $400 
+                million in construction projects working directly for owners. Historic preservation work has allowed Galanis to work on buildings designed by some of the greatest American architects. The Somerset Club, and 39-40 Beacon Street projects restored buildings originally designed by Alexander Parris in the mid 1800s. Projects at Phillips Academy Andover restored buildings originally designed by Asher Benjamin, Charles Bulfinch, Charles Platt, and Guy Lowell. In addition, sensitive landscape design and work has taken place on this Frederick Law Olmstead campus.
             </p>
         </div>
     </div>
